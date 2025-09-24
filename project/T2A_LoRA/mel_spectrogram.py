069ec6f169656d8f43fc4337f8ce2ab803f00835
이신재 <sinjae2006@gmail.com>
2025-09-24 13:42:18 +0900

    feat: add project\T2A_LoRA\mel_spectrogram.py

---
+++ project/T2A_LoRA/mel_spectrogram.py
@@ -0,0 +1,19 @@
+import os
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_mel_spectrogram(y, sr, n_mels=128, fmax=8000):
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, fmax=fmax)
+    S_dB = librosa.power_to_db(S, ref=np.max)
+    plt.figure(figsize=(10, 6))
+    librosa.display.specshow(S_dB, sr=sr, x_axis='time', y_axis='mel', fmax=fmax)
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel-frequency spectrogram')
+    plt.show()
+
+if __name__ == "__main__":
+    for audio_path in os.listdir('.'):
+        if audio_path.endswith('.wav'):
+            y, sr = librosa.load(audio_path, sr=44100)
+            plot_mel_spectrogram(y, sr)
